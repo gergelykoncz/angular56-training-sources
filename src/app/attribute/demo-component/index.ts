a6d44d612ada78ee8464a41436d2e8c8523a1dc8
Gergely Koncz <gergo.koncz@gmail.com>
2018-06-23 13:44:54 +0200

    Enabling debug tools

--- src/app/attribute/demo-component/index.ts
+++ src/app/attribute/demo-component/index.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { IncrementService } from '../../core';
 
 @Component({
   selector: 'app-attribute-demo',
   templateUrl: './ui.html'
 })
-export class AttributeDemoComponent {}
+export class AttributeDemoComponent {
+  constructor(private service: IncrementService) {
+    console.log(service.counter);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, ApplicationRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { enableDebugTools } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(module => {
+    const appRef = module.injector.get(ApplicationRef);
+    const appComponent = appRef.components[0];
+    enableDebugTools(appComponent);
+  })
   .catch(err => console.log(err));
